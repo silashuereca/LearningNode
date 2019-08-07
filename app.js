8f45a25a14eb37716c0b868758167b94a234de64
silashuereca <silasgranthuereca@gmail.com>
2019-08-07 16:55:30 -0500

    added in new js for export module

---
+++ app.js
@@ -0,0 +1,6 @@
+// Import JS modules
+
+// const tutorial = require('./tutorial')
+// console.log(tutorial.sum(2,3));
+// console.log(tutorial.PI);
+// console.log(new tutorial.SomeMathObject());
---
+++ tutorial.js
@@ -0,0 +1,12 @@
+// Export Module
+
+// const sum = (num1, num2) => num1 + num2;
+// const PI = 3.14;
+// class SomeMathObject {
+//     constructor() {
+//         console.log('object created')
+//     }
+// }
+// module.exports.sum = sum;
+// module.exports.PI = PI;
+// module.exports.SomeMathObject = SomeMathObject;
