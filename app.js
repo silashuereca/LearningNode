b6b183968f294b70cb26111424b095f55202d7c4
silashuereca <silasgranthuereca@gmail.com>
2019-08-09 18:03:05 -0500

    add File system module

--- app.js
+++ app.js
@@ -85,5 +85,24 @@
 
 
 
-// Working with File System Module
+// Working with the File System Module
 
+const fs = require('fs');
+
+// create file 
+fs.writeFile('example.txt', "this is an example", (err) => {
+    if(err)
+        console.log(err)
+     else {
+        console.log('File successfully created')
+        fs.readFile('example.txt','utf8', (err, file) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(file)
+            }
+        })
+     }
+       
+    
+});
