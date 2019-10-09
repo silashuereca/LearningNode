94025a67258cab9b350b911a21cb56470f63a32d
silashuereca <silasgranthuereca@gmail.com>
2019-10-08 22:53:48 -0500

    add new node files

---
+++ events/events.js
@@ -0,0 +1,21 @@
+// let fs = require('fs');
+
+// let readStream = fs.createReadStream('./demofile.txt');
+
+// readStream.on('open', () => {
+// 	console.log('Hello there');
+// });
+
+// let events = require('events');
+// let eventEmitter = new events.EventEmitter();
+
+// //create event handler
+// let myEventEmitter = () => {
+// 	console.log('I hear a scream');
+// };
+
+// // Assign the eventhandler to an event
+// eventEmitter.on('scream', myEventEmitter);
+
+// // fire the 'scream' event
+// eventEmitter.emit('scream');
